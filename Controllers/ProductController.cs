cdd425b27dd6bea2620a55436b801d47914bc916
Carlos Diaz <carlos.cedp@gmail.com>
2022-06-29 09:59:06 -0400

    added more methods to controller

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using AdvWorksAPI.DTOs;
+﻿using AdvWorksAPI.DataAccessEF6;
+using AdvWorksAPI.DTOs;
 using AdvWorksAPI.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
@@ -26,6 +27,31 @@ namespace AdvWorksAPI.Controllers
             var data = _productRepository.GetProducts();
             var result = _mapper.Map<IEnumerable<ProductDTO>>(data);
             return Ok(result);
+        }
+        [HttpGet("{Id:int}")]
+        public IActionResult Get(int Id)
+        {
+            if (!_productRepository.ProductExists(Id)) return NotFound();
+
+            var product = _productRepository.GetProduct(Id);
+            var response = _mapper.Map<ProductDTO>(product);
+            return Ok(response);
+        }
+        [HttpPost]
+        public IActionResult Create([FromBody] ProductDTO body)
+        {
+            var product = _mapper.Map<Product>(body);
+            _productRepository.AddProduct(product);
+            _productRepository.Save();
+            return StatusCode(201);
+        }
+        [HttpDelete("{Id:int}")]
+        public IActionResult Delete([FromBody] ProductDTO body)
+        {
+            if (!_productRepository.ProductExists(body.ProductID)) return NotFound();
+            _productRepository.DeleteProduct(new Product { ProductID = body.ProductID });
+            _productRepository.Save();
+            return Ok();
         }
     }
 }
